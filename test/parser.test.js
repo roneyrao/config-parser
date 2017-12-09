24b014f557f463dcd20fbfaa9768ba4291661fc9
Roney <n.rao@hnair.com>
2017-12-09 12:33:48 +0800

    lint, coverage

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,29 +1,53 @@
-import {expect} from 'chai';
+import {
+  expect,
+} from 'chai';
+
 import parser from '../index';
-const cfg=require('require-json5')(require('path').resolve(__dirname, '.cfg.json5'));
 
-describe('returns default config', function(){
-	const defs=Object.assign({}, cfg);
-	delete defs.targets;
-	for (const k in defs) {
-		defs[['__',k.toUpperCase(),'__'].join('')] = defs[k];
-		delete defs[k];
-	}
-	const parsed=parser(cfg);
+const cfg = require('require-json5')(require('path').resolve(__dirname, '.cfg.json5'));
 
-	it('returns correct default config', function(){
-		expect(parsed.cfg).to.eql(defs);
-	})
-	it('returns correct stringified strings', function(){
-		expect(parsed.stringifiedCfg.__PUBLIC_PATH__).to.equal('"/package/"');
-	})
-})
-describe('selects mock target', function(){
-	const parsed=parser(cfg, 'test');
-	it('returns correct public path', function(){
-		expect(parsed.cfg.__PUBLIC_PATH__).to.equal('/root/');
-	})
-	it('returns correct api path', function(){
-		expect(parsed.cfg.__API_PATH__).to.equal('https://192.12.33.44');
-	})
-})
+describe('returns default config', () => {
+  const defs = Object.assign({}, cfg);
+  delete defs.targets;
+  Object.entries(defs).forEach(([k, v]) => {
+    defs[['__', k.toUpperCase(), '__'].join('')] = v;
+    delete defs[k];
+  });
+  const parsed = parser(cfg);
+
+  it('returns correct default config', () => {
+    expect(parsed.cfg).to.eql(defs);
+  });
+  it('returns correct stringified strings', () => {
+    expect(parsed.stringifiedCfg.__PUBLIC_PATH__).to.equal('"/package/"');
+  });
+  it('returns correct target', () => {
+    expect(parsed.target).to.be.undefined;
+  });
+});
+describe('selects mock target', () => {
+  const parsed = parser(cfg, 'test');
+  it('returns correct public path', () => {
+    expect(parsed.cfg.__PUBLIC_PATH__).to.equal('/root/');
+  });
+  it('returns correct api path', () => {
+    expect(parsed.cfg.__API_PATH__).to.equal('https://192.12.33.44');
+  });
+  it('returns correct target', () => {
+    expect(parsed.target).to.equal('test');
+  });
+});
+describe('selects non-exist target', () => {
+  it('throws error', () => {
+    expect(() => parser(cfg, 'abcd')).to.throw('unfound');
+  });
+});
+describe('force to production', () => {
+  it('throws error', () => {
+    const env = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+    expect(parser(cfg, 'test').target).to.equal('production');
+    process.env.NODE_ENV = env;
+  });
+});
+
